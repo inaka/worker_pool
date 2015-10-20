d08367b4bb440105e1bda92ad5749559786ceb47
X4lldux <xalldux@gmail.com>
2015-10-20 19:48:51 +0200

    Add `worerk_opt` to `DEFAULTS`

--- src/wpool.erl
+++ src/wpool.erl
@@ -20,6 +20,7 @@
 -define(DEFAULTS, [ {overrun_warning, infinity}
                   , {overrun_handler, {error_logger, warning_report}}
                   , {workers, 100}, {worker, {wpool_worker, undefined}}
+                  , {worker_opt, []}
                   ]).
 
 -type name() :: atom().
