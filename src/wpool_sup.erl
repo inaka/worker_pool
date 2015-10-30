ce010cf91d5e15370e1ac84eb22c57d2c24f9de5
Brujo Benavides <elbrujohalcon@inaka.net>
2015-10-30 13:05:34 -0300

    [Fix #35] Change wpool_sup restart strategy to permanent

--- src/wpool_sup.erl
+++ src/wpool_sup.erl
@@ -49,4 +49,4 @@ init([]) ->
   ok = wpool_pool:create_table(),
   {ok, {{simple_one_for_one, 5, 60},
         [{wpool_pool, {wpool_pool, start_link, []},
-         transient, 2000, supervisor, dynamic}]}}.
+         permanent, 2000, supervisor, dynamic}]}}.
