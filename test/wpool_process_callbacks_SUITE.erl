bdd3975e27680f370c1f7fd4a33d05bedb2057d0
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2018-10-11 11:59:57 +0200

    test that crashing callback does not affect others

--- test/wpool_process_callbacks_SUITE.erl
+++ test/wpool_process_callbacks_SUITE.erl
@@ -10,6 +10,7 @@
 -export([ complete_callback_passed_when_starting_pool/1
         , partial_callback_passed_when_starting_pool/1
         , callback_can_be_added_and_removed_after_pool_is_started/1
+        , crashing_callback_does_not_affect_others/1
         ]).
 
 -spec all() -> [atom()].
@@ -17,6 +18,7 @@ all() ->
   [ complete_callback_passed_when_starting_pool
   , partial_callback_passed_when_starting_pool
   , callback_can_be_added_and_removed_after_pool_is_started
+  , crashing_callback_does_not_affect_others
   ].
 
 -spec init_per_suite(config()) -> config().
@@ -30,7 +32,7 @@ end_per_suite(Config) ->
   Config.
 
 
--spec complete_callback_passed_when_starting_pool(config) -> ok.
+-spec complete_callback_passed_when_starting_pool(config()) -> ok.
 complete_callback_passed_when_starting_pool(_Config) ->
   Pool = callbacks_test,
   WorkersCount = 13,
@@ -72,9 +74,9 @@ partial_callback_passed_when_starting_pool(_Config) ->
 
   ok.
 
--spec callback_can_be_added_and_removed_after_pool_is_started(config) -> ok.
+-spec callback_can_be_added_and_removed_after_pool_is_started(config()) -> ok.
 callback_can_be_added_and_removed_after_pool_is_started(_Config) ->
-  Pool = partial_callbacks_test,
+  Pool = after_start_callbacks_test,
   WorkersCount = 3,
   meck:new(callbacks, [non_strict]),
   meck:expect(callbacks, on_worker_dead, fun(_AWorkerName, _Reason) -> ok end),
@@ -108,3 +110,27 @@ callback_can_be_added_and_removed_after_pool_is_started(_Config) ->
   meck:unload(callbacks2),
 
   ok.
+
+
+-spec crashing_callback_does_not_affect_others(config()) -> ok.
+crashing_callback_does_not_affect_others(_Config) ->
+  Pool = crashing_callbacks_test,
+  WorkersCount = 3,
+  meck:new(callbacks, [non_strict]),
+  meck:expect(callbacks, on_new_worker, fun(_AWorkerName) -> ok end),
+  meck:new(callbacks2, [non_strict]),
+  meck:expect(callbacks2, on_new_worker, fun(_AWorkerName) -> error(not_going_to_work) end),
+  {ok, _Pid} = wpool:start_pool(Pool, [{workers, WorkersCount},
+                                       {worker, {crashy_server, []}},
+                                       {callbacks, [callbacks, callbacks2]}]),
+
+  timer:sleep(100),
+  WorkersCount = meck:num_calls(callbacks, on_new_worker, ['_']),
+  WorkersCount = meck:num_calls(callbacks2, on_new_worker, ['_']),
+
+  wpool:stop_pool(Pool),
+  meck:unload(callbacks),
+  meck:unload(callbacks2),
+
+  ok.
+
